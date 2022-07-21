b513fefe38c6105977aeda550d32c314c2a6bbc4
jvisser <jvisser3@gmail.com>
2022-07-21 16:15:49 +0200

    Add Tiled map file for wilderness

---
+++ assets/map/tileset/height.tsx
@@ -0,0 +1,329 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="height" tilewidth="8" tileheight="8" tilecount="65" columns="65">
+ <image source="height.tmx" width="520" height="8"/>
+ <tile id="0">
+  <properties>
+   <property name="height" type="int" value="33280"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="height" type="int" value="33016"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="height" type="int" value="33008"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="height" type="int" value="33000"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="height" type="int" value="32992"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="height" type="int" value="32984"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="height" type="int" value="32976"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="height" type="int" value="32968"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="height" type="int" value="32960"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="height" type="int" value="32952"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="height" type="int" value="32944"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="height" type="int" value="32936"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="height" type="int" value="32928"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="height" type="int" value="32920"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="height" type="int" value="32912"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="height" type="int" value="32904"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="height" type="int" value="32896"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="height" type="int" value="32888"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="height" type="int" value="32880"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="height" type="int" value="32872"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="height" type="int" value="32864"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="height" type="int" value="32856"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="height" type="int" value="32848"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="height" type="int" value="32840"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="height" type="int" value="32832"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="height" type="int" value="32824"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="height" type="int" value="32816"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="height" type="int" value="32808"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="height" type="int" value="32800"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="height" type="int" value="32792"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="height" type="int" value="32784"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="height" type="int" value="32776"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="height" type="int" value="32768"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="height" type="int" value="32760"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="height" type="int" value="32752"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="height" type="int" value="32744"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="height" type="int" value="32736"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="height" type="int" value="32728"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="height" type="int" value="32720"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="height" type="int" value="32712"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="height" type="int" value="32704"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="height" type="int" value="32696"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="height" type="int" value="32688"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="height" type="int" value="32680"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="height" type="int" value="32672"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="height" type="int" value="32664"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="height" type="int" value="32656"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="height" type="int" value="32648"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="height" type="int" value="32640"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="height" type="int" value="32632"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="height" type="int" value="32624"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="height" type="int" value="32616"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="height" type="int" value="32608"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="height" type="int" value="32600"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="height" type="int" value="32592"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="height" type="int" value="32584"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="height" type="int" value="32576"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="height" type="int" value="32568"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="height" type="int" value="32560"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="height" type="int" value="32552"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="height" type="int" value="32544"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="height" type="int" value="32536"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="height" type="int" value="32528"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="height" type="int" value="32520"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="height" type="int" value="32256"/>
+  </properties>
+ </tile>
+</tileset>
