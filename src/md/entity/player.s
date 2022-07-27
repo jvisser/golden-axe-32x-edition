d0dfb21fd299904a6916e2348e1836026e244728
jvisser <jvisser3@gmail.com>
2022-07-27 19:18:45 +0200

    Disable auto walk if initial player position is on screen

---
+++ src/md/entity/player.s
@@ -0,0 +1,36 @@
+|--------------------------------------------------------------------
+| Player character patches
+|--------------------------------------------------------------------
+
+    .include "goldenaxe.i"
+    .include "patch.i"
+
+
+    |-------------------------------------------------------------------
+    | Patch initial auto walk animation to only occur if the current/starting player position is offscreen to the left
+    |-------------------------------------------------------------------
+    patch_start 0x0014bc
+        jsr     players_init
+    patch_end
+
+    players_init:
+        lea     (entity_player_1), %a0
+        bsr     init_player_auto_walk
+        lea     (entity_player_2), %a0
+        bsr     init_player_auto_walk
+        rts
+
+    init_player_auto_walk:
+        | Check if the current player state is auto walk
+        cmp.b   #0x68, ENTITY_STATE(%a0)
+        bne     .exit
+
+        | Auto walk if outside of the left horizontal screen boundary
+        cmp.w   #0x80, ENTITY_X(%a0)
+        bcs     .exit
+
+        | Skip auto walk
+        clr.b   ENTITY_STATE(%a0)
+
+    .exit:
+        rts
