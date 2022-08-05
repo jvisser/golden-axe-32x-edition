b7cbc4a4091c49ea5212704059813acc77d4cc69
jvisser <jvisser3@gmail.com>
2022-08-05 19:37:16 +0200

    Add some more cheats

--- src/md/engine/cheat.s
+++ src/md/engine/cheat.s
@@ -6,6 +6,9 @@
 
 
 #define CHEAT_ENEMY_HP
+#define CHEAT_NO_MAGIC_CONSUMPTION
+//#define CHEAT_MAGIC_START_COUNT         9
+#define CHEAT_GOD_MODE
 
 
 #ifdef CHEAT_ENEMY_HP
@@ -16,6 +19,48 @@
     patch_start 0x00d3d2
         move.b  #1, 0x64(%a0)
         rts
+    patch_end
+
+#endif
+
+
+#ifdef CHEAT_NO_MAGIC_CONSUMPTION
+
+    /**********************************************************
+     * Don't clear flask count after magic use
+     */
+    patch_start 0x007c5c
+        nop
+    patch_end
+
+#endif
+
+
+#ifdef CHEAT_MAGIC_START_COUNT
+
+    /**********************************************************
+     * Start with n amount of flasks
+     */
+    patch_start 0x004260
+        .dc.b   CHEAT_MAGIC_START_COUNT
+    patch_end
+
+#endif
+
+
+#ifdef CHEAT_GOD_MODE
+
+    /**********************************************************
+     * Can't die
+     */
+    patch_start 0x006ebe    // Damage
+        nop
+        nop
+    patch_end
+
+    patch_start 0x007d5c    // Fall death
+        nop
+        nop
     patch_end
 
 #endif
